ebd5ab0f0b90c8b870a50616ff7b8f1ad5a9bfd4
Nagireddy P <nagireddy.panditi@gmail.com>
2020-02-24 04:10:44 +0530

    Updates

---
+++ precache-manifest.7ef39c3faab5fc6fe794a9fd985e4aee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f09bee15c66daca1b50ca591b83c7c",
+    "url": "/local_venue/index.html"
+  },
+  {
+    "revision": "a9e255b2accea2107311",
+    "url": "/local_venue/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "352b044684a94ec058d5",
+    "url": "/local_venue/static/js/2.07e376fd.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/local_venue/static/js/2.07e376fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e255b2accea2107311",
+    "url": "/local_venue/static/js/main.abce2cf0.chunk.js"
+  },
+  {
+    "revision": "07644408b72c3d56c18e",
+    "url": "/local_venue/static/js/runtime-main.0e08349a.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/local_venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/local_venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/local_venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
